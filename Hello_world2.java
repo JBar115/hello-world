24c4233b44ac4fbee9a335aec3d6b11be8afcdd3
JBar115 <jbarrera617@outlook.com>
2024-09-09 17:54:49 -0400

    New print statement
    added a new print statement at the 6th line.
    no other changes

--- Hello_world2.java
+++ Hello_world2.java
@@ -2,6 +2,7 @@ public class Hello_world2
 {
    public static void main (String[] args)
    {
-      System.out.println ("hello World!");       
+      System.out.println ("hello World!");   
+       System.out.println ("Program modified, but able to revert to original version using GitHub Desktop");   
    }
 }
