20c93c79fe9fcb84ba879d8576ac4529fb3a416f
JBar115 <jbarrera617@outlook.com>
2024-09-10 18:58:17 -0400

    Revert "New print statement"
    This reverts commit 24c4233b44ac4fbee9a335aec3d6b11be8afcdd3.

--- Hello_world2.java
+++ Hello_world2.java
@@ -2,7 +2,6 @@ public class Hello_world2
 {
    public static void main (String[] args)
    {
-      System.out.println ("hello World!");   
-       System.out.println ("Program modified, but able to revert to original version using GitHub Desktop");   
+      System.out.println ("hello World!");       
    }
 }
